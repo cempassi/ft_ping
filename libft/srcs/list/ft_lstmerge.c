847518cce013b481113533f5788b862f3ff2e161
Cedric M'Passi <cedricmpassi@gmail.com>
2020-11-02 03:25:01 +0100

    Delete submodule

---
+++ libft/srcs/list/ft_lstmerge.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmerge.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/23 03:12:23 by cempassi          #+#    #+#             */
+/*   Updated: 2020/07/23 03:12:23 by cempassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmerge(t_list **alst, t_list *blst)
+{
+	t_list *holder;
+
+	holder = *alst;
+	if (*alst == NULL)
+	{
+		*alst = blst;
+		return (*alst);
+	}
+	ft_lstaddback(alst, blst);
+	return (*alst);
+}
